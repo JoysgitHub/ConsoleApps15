f716b1ff1f1eab04d716d6d66e852642e1232a69
JoysgitHub <114746470+JoysgitHub@users.noreply.github.com>
2023-03-14 05:59:00 +0000

    -Test Driven Dev

--- ConsoleAppProject/App03/StudentGrades.cs
+++ ConsoleAppProject/App03/StudentGrades.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using ConsoleAppProject.Helpers;
 
 namespace ConsoleAppProject.App03
@@ -11,6 +12,72 @@ namespace ConsoleAppProject.App03
     /// </summary>
     public class StudentGrades
     {
+        public string[] Students { get; set; }
+        public int[] Marks { get; set; }
 
-    }
+        public int[] GradeProfile { get; set; }
+
+        public double Mean { get; set; }
+
+        public int Minimum { get; set; }
+
+        public int Maximum { get; set; }
+
+
+        public StudentGrades()
+        {
+            Students = new string[]
+                 {
+            "Daniel", "Dylan", "Eric",
+            "Georgia", "Hasan", "Hamza",
+            "Jack", "Liam", "Shan",
+                     "Shamial"
+            };
+
+
+            GradeProfile = new int[(int)Grades.A + 1];
+            Marks = new int[Students.Length];
+        }
+
+
+        public void InputMarks()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// List all the students and display their /// name and current mark
+        /// </summary>
+
+        public void OutputMarks()
+        {
+            throw new NotImplementedException();
+        }
+
+        ﻿
+        /// <summary>
+        /// Convert a student mark to a grade /// from F (Fail) to A (First Class) /// </summary>
+       
+        public Grades ConvertToGrade(int mark)
+        {
+            throw new NotImplementedException();
+        }
+        
+        /// <summary>
+        /// Calculate and display the minimum, maximum /// and mean mark for all the students
+        /// </summary>
+       
+        public void CalculateStats()
+        {
+         throw new NotImplementedException();
+        }
+
+
+
+
+
+
+
+
+    }      
 }
